f91603f80d06bb81366f3ecfe6e019636f197e73
IMV2811 <116003727+IMV2811@users.noreply.github.com>
2023-01-12 15:21:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://dulink.in/st?api'
+    url = f'https://du-link.in/st?'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
